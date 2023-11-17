7f49a8356f2d27fee970a0a3d24046f14cf4f375
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-17 15:50:40 +0100

    Update exercise

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('subtitle').innerHTML = 'Benvenuto' 
+
+let name = prompt('Inserisci il tuo nome')
+
+let surname = prompt('Inserisci il tuo congome')
+
+let colore_preferito = prompt('Inserisci il tuo colore preferito')
+
+let persona = name + ' ' + surname + ' ' + colore_preferito;
+
+document.getElementById('subtitle').innerHTML = document.getElementById('subtitle').innerHTML + ' ' + persona;
+
