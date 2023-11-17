3679344b7d9f1042b9b4455c4acf38a0d00ab54e
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-17 16:56:09 +0100

    Corrected the exercise

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ let surname = prompt('Inserisci il tuo congome')
 
 let colore_preferito = prompt('Inserisci il tuo colore preferito')
 
-let persona = name + ' ' + surname + ' ' + colore_preferito;
+let persona = name + surname + colore_preferito;
 
-document.getElementById('subtitle').innerHTML = document.getElementById('subtitle').innerHTML + ' ' + persona;
+document.getElementById('subtitle').innerHTML = document.getElementById('subtitle').innerHTML + ' ' + name +
+" la tua password è: " +persona + "23";
 
